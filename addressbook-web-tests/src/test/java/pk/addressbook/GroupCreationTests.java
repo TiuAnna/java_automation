92ba0509ea3276ac71a51df6ee0c21b3a9574f1b
TiuAnna <tiulk.anna@gmail.com>
2022-10-23 08:37:40 +0300

    Test for group creation is modified
    Test class was renamed. Test actions were divided into methods.

--- addressbook-web-tests/src/test/java/pk/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pk/addressbook/GroupCreationTests.java
@@ -8,7 +8,6 @@ import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 
 import java.util.*;
@@ -22,29 +21,50 @@ public class GroupCreationTests {
     driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
+    driver.get("http://localhost/addressbook/");
+    /*driver.manage().window().setSize(new Dimension(550, 691));*/
+    login("admin", "secret");
   }
+
+  private void login(String username, String password) {
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.id("LoginForm")).click();
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+  }
+
   @After
   public void tearDown() {
     driver.quit();
   }
   @Test
-  public void test1() {
-    driver.get("http://localhost/addressbook/");
-    driver.manage().window().setSize(new Dimension(550, 691));
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("aaa");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("bbb");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("ccc");
+  public void testGroupCreation() {
+    goToGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("new group", "new header", "new footer"));
+    submitGroupCreation();
+    goToGroupPage();
+  }
+
+  private void submitGroupCreation() {
     driver.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).sendKeys(groupData.name());
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).sendKeys(groupData.header());
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.footer());
+  }
+
+  private void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage() {
     driver.findElement(By.linkText("groups")).click();
   }
 }
