afe98fd3d965e630d12787a5b48d492ba7911ee5
TiuAnna <tiulk.anna@gmail.com>
2022-11-16 06:52:04 +0300

    gitignore

--- addressbook-web-tests/src/test/java/pk/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pk/addressbook/tests/GroupDeletionTests.java
@@ -22,5 +22,10 @@ public class GroupDeletionTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
 
+        before.remove(before.size() - 1);
+        for (int i = 0; i < after.size(); i++) {
+            Assert.assertEquals(before.get(i), after.get(i));
+        }
+
     }
 }
