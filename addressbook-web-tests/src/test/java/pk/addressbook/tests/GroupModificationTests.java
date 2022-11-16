a4caeeb3729c6f66e965b19bbc8add1999d38235
TiuAnna <tiulk.anna@gmail.com>
2022-11-16 07:26:20 +0300

    gitignore

--- addressbook-web-tests/src/test/java/pk/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pk/addressbook/tests/GroupModificationTests.java
@@ -4,22 +4,28 @@ import org.junit.Test;
 import org.testng.Assert;
 import pk.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().goToGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()) {
+        if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("new group", "new header", "new footer"));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("modified group", "new header", "modified footer"));
+        GroupData group = new GroupData("modified group", "new header", "modified footer");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
 }
